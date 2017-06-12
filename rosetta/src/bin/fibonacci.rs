a9637de73cb9aa1977f3c9ecd319fbe6a1c1eb4b
Matt Gathu <mattgathu@gmail.com>
2017-06-12 21:35:34 +0300

    fibonacci sequence

---
+++ rosetta/src/bin/fibonacci.rs
@@ -0,0 +1,34 @@
+// In mathematics, the Fibonacci numbers are the numbers in the 
+// Fibonacci sequence, characterized by the fact that every number 
+// after the first two is the sum of the two preceding ones.
+//
+// Task
+// Write a function to generate the   nth   Fibonacci number. 
+//
+// http://rosettacode.org/wiki/Fibonacci_sequence
+
+fn fib(n: i64) -> i64 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fib(n-1) + fib(n-2),
+    }
+}
+
+
+fn main() {
+    println!("fib(5): {}", fib(5));
+}
+
+
+#[test]
+fn test_fib() {
+    assert_eq!(fib(1), 1);
+    assert_eq!(fib(2), 1);
+    assert_eq!(fib(3), 2);
+    assert_eq!(fib(4), 3);
+    assert_eq!(fib(5), 5);
+    assert_eq!(fib(6), 8);
+    assert_eq!(fib(7), 13);
+    assert_eq!(fib(9), 34);
+}
